16abd44ef3636f387d29ce3d8a61f870cc7a7535
kadiebwe oscar <kadiebweoscar0@gmail.com>
2023-10-20 02:03:22 +0100

    display a date and hours

--- calendar&CalculAge.js
+++ calendar&CalculAge.js
@@ -1,8 +1,15 @@
 const itemHeure = document.getElementById("heure");
 const itemMinute = document.getElementById("minute");
 const itemSeconde = document.getElementById("seconde");
+const itemJour = document.getElementById("jour");
+const itemMois = document.getElementById("mois");
+const itemAnnée = document.getElementById("année");
+
 let myDate = new Date();
 
 itemHeure.innerHTML += myDate.getHours();
 itemMinute.innerHTML += myDate.getMinutes();
 itemSeconde.innerHTML += myDate.getSeconds();
+itemJour.innerHTML += myDate.getDay();
+itemMois.innerHTML += myDate.getMonth();
+itemAnnée.innerHTML += myDate.getFullYear();
